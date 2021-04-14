b1799c5dcf58714d8f1c20bec4682545319ef5d1
Daniel Pfisterer <daniel.pfisterer@power.cloud>
2021-04-14 11:52:51 +0200

    creta a basic working cli

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,3 +1,11 @@
-export const cli = (args: string[]) => {
+import prompt from 'prompt';
+
+export const cli = async (args: string[]) => {
   console.log(args);
+  prompt.start();
+
+  const { username, email } = await prompt.get(['username', 'email']);
+
+  console.log('  username: ' + username);
+  console.log('  email: ' + email);
 };
