c7894201dfb5aabe2c1d5c5e0b39a185078220e2
pure180 <info@daniel-pfisterer.de>
2021-12-31 12:10:20 +0400

    Use full linter and ts-config

--- packages/template/.eslintrc.js
+++ packages/template/.eslintrc.js
@@ -1,3 +1,28 @@
 module.exports = {
-  extends: '../../.eslintrc.js',
+  parser: '@typescript-eslint/parser',
+  root: true,
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/explicit-member-accessibility': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/no-parameter-properties': 0,
+    '@typescript-eslint/interface-name-prefix': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
+    '@typescript-eslint/no-explicit-any': 'off',
+    'import/extensions': 'off',
+    'import/no-unresolved': 'off',
+    'no-restricted-syntax': 'off',
+    'no-await-in-loop': 'off',
+    'consistent-return': 'off',
+  },
 };
